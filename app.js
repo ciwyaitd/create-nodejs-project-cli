11aa8339b939f865a8cf9c86a038afa9a624fd0c
LiHang Yang <863141163@qq.com>
2018-10-02 16:41:32 +0800

    version patch 1.1.1

--- app.js
+++
@@ -1,8 +0,0 @@
-const ora = require('ora')
-const childProcess = require('child_process')
-childProcess.exec('echo hello')
-const spinner = ora('Loading unicorns').start()
-
-setTimeout(() => {
-  spinner.succeed()
-}, 1000)
